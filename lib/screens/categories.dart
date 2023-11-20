9300b73aed7d39b6077fd4c04dbf2e0d25e7280e
unknown <riyadrich22@gmail.com>
2023-11-20 10:00:53 +0100

    loading recipes from list & making app looks better overall

---
+++ lib/screens/categories.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+import 'package:meals/data/data.dart';
+import 'package:meals/models/category.dart';
+import 'package:meals/screens/meals.dart';
+import 'package:meals/widgets/category_grid_item.dart';
+
+class CatagoriesScreen extends StatelessWidget {
+  const CatagoriesScreen({super.key});
+
+  void _selectCategory(BuildContext context, Category category) {
+    final filteredMeals = dummyMeals
+        .where((meal) => meal.categories.contains(category.id))
+        .toList();
+    Navigator.push(
+      context,
+      MaterialPageRoute(
+        builder: ((context) =>
+            MealsScreen(title: category.title, meals: filteredMeals)),
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Pick your catagory'),
+      ),
+      body: GridView(
+        padding: const EdgeInsets.all(24),
+        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 2,
+          childAspectRatio: 3 / 2,
+          crossAxisSpacing: 20,
+          mainAxisSpacing: 20,
+        ),
+        children: [
+          for (final category in availableCategories)
+            CategoryGridItem(
+              category: category,
+              onSelectCategory: () {
+                _selectCategory(context, category);
+              },
+            )
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/screens/meals.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+import 'package:meals/models/meal.dart';
+import 'package:meals/widgets/meal_item.dart';
+
+class MealsScreen extends StatelessWidget {
+  const MealsScreen({super.key, required this.title, required this.meals});
+  final String title;
+  final List<Meal> meals;
+
+  @override
+  Widget build(BuildContext context) {
+    Widget content = ListView.builder(
+      itemCount: meals.length,
+      itemBuilder: (ctx, index) => MealItem(meal: meals[index]),
+    );
+
+    if (meals.isEmpty) {
+      content = Center(
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Text(
+              ' Nothing here!',
+              style: Theme.of(context)
+                  .textTheme
+                  .headlineLarge!
+                  .copyWith(color: Theme.of(context).colorScheme.onBackground),
+            ),
+            const SizedBox(
+              height: 16,
+            ),
+            Text(
+              'Try selecting a diffrent category',
+              style: Theme.of(context)
+                  .textTheme
+                  .bodyLarge!
+                  .copyWith(color: Theme.of(context).colorScheme.onBackground),
+            ),
+          ],
+        ),
+      );
+    }
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(title),
+      ),
+      body: content,
+    );
+  }
+}
